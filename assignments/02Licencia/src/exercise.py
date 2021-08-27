d5432c5ada32d3fcd738b836f7ac1890d2cefca8
Patricio Vélez <a00833485@itesm.mx>
2021-08-26 19:06:17 -0500

    Terminados v1

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -2,6 +2,18 @@
 def main():
     edad = int(input("Ingresa tu edad: "))
     #Aquí empieza tu programa...
+    if edad >= 18:
+        id= (input("¿Tienes identificación oficial? (s/n): "))
+        if id == "s":
+            print("Trámite de licencia concedido")
+        elif id == "n":
+            print("No cumples requisitos")
+        else:
+            print("Respuesta incorrecta")
+    elif edad < 18 and edad > 0:
+        print("No cumples requisitos")
+    else:
+        print("Respuesta incorrecta")
 
 if __name__ == '__main__':
     main()
--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,6 +1,21 @@
 def main():
     #escribe tu código abajo de esta línea
-    
+    peso = float(input("Peso en kg: "))
+    altura = float(input("Altura en m: "))
+    if peso > 0 and altura > 0:
+        indice = peso / altura**2
+        if indice < 20:
+            print("PESO BAJO")
+        if indice >= 20 and indice < 25:
+            print("NORMAL")
+        if indice >= 25 and indice < 30:
+            print("SOBREPESO")
+        if indice >= 30 and indice < 40:
+            print("OBESIDAD")
+        if indice >= 40:
+            print("OBESIDAD MORBIDA")
+    else:
+        print("Revisa tus datos, alguno de ellos es erróneo.")
 
 if __name__=='__main__':
     main()
